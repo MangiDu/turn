ca18653f9b6a63347c52cee07b0da1d6ed9f266d
Mangi <duxiaoxue93@gmail.com>
2017-11-09 18:49:15 +0800

    modified document title

--- src/app.jsx
+++ src/app.jsx
@@ -24,6 +24,7 @@ class App extends React.Component {
   }
   componentDidMount () {
     user.request().then((data) => {
+      document.title = data.name
       this.setState({
         user: user.getData()
       })
